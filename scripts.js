8e1e2e620908363f3e3085aa643ed72207e22ca3
Nusret Butunay <nusret.butunay@justappraised.com>
2024-08-07 23:36:04 +0300

    add more

--- scripts.js
+++ scripts.js
@@ -1,12 +1,31 @@
+// Show or hide the button
+window.onscroll = function() {
+    const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        scrollToTopBtn.style.display = 'flex';
+    } else {
+        scrollToTopBtn.style.display = 'none';
+    }
+};
+
+// Scroll to the top function
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
 function searchDream() {
     const searchInput = document.getElementById('search-input').value.toLowerCase();
     const articles = document.querySelectorAll('article');
-
     let found = false;
+
     articles.forEach(article => {
         const title = article.querySelector('h3').innerText.toLowerCase();
         if (title.includes(searchInput)) {
             article.scrollIntoView({ behavior: 'smooth' });
+            article.classList.add('highlight');
+            setTimeout(() => {
+                article.classList.remove('highlight');
+            }, 3000); // Remove the highlight after 3 seconds
             found = true;
         }
     });
@@ -15,3 +34,4 @@ function searchDream() {
         alert('Aradığınız rüya tabiri bulunamadı.');
     }
 }
+
